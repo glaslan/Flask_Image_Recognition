552d9a1a921a4b7ab0076af66cd260b2c5074f20
glaslan <glasd6702@gmail.com>
2025-10-12 20:11:44 -0400

    Modify model.py to adhear to pylint recommendations

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+'''
+This module handles image preprocessing and digit prediction using a trained Keras model.
+'''
+
 # Importing required libs
 from keras.models import load_model
 from keras.utils import img_to_array
@@ -10,6 +14,15 @@ model = load_model("digit_model.h5")
 
 # Preparing and pre-processing the image
 def preprocess_img(img_path):
+    """
+    Preprocess an image for model prediction.
+
+    Args:
+        img_path: File path or stream to the image.
+
+    Returns:
+        numpy.ndarray: Preprocessed image array of shape (1, 224, 224, 3).
+    """
     op_img = Image.open(img_path)
     img_resize = op_img.resize((224, 224))
     img2arr = img_to_array(img_resize) / 255.0
@@ -19,5 +32,14 @@ def preprocess_img(img_path):
 
 # Predicting function
 def predict_result(predict):
+    """
+    Predict the digit from a preprocessed image.
+
+    Args:
+        predict (numpy.ndarray): Preprocessed image array.
+
+    Returns:
+        int: Predicted digit (0-9).
+    """
     pred = model.predict(predict)
     return np.argmax(pred[0], axis=-1)
